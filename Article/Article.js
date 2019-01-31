d837c8b1967bbf8c4f09d37e4eb142c64f5c3a90
t-tullis <t.tullis34@gmail.com>
2019-01-31 11:27:53 -0800

    completed drop down portion

--- Article/Article.js
+++ Article/Article.js
@@ -1,26 +1,42 @@
 // Because classes are not hoised you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
+let tl = new TimelineMax();
 
 class Article {
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
-    console.log(this.domElement);
+
+    
     // create a reference to the ".expandButton" class. 
     this.expandButton = domElement.querySelector('.expandButton')
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = 'expand'
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     this.expandButton.addEventListener('click', () => this.expandArticle());
-  }
 
+    //Hover over and off
+    this.expandButton.addEventListener('mouseover', (e) =>{
+      TweenMax.to(e.target, .7, {opacity: .6, scale: 3, y: -10})
+    })
+    this.expandButton.addEventListener('mouseout', (e) =>{
+      TweenMax.to(e.target, .5, {opacity: 1, scale: 1, y: 0})
+    })
+  }
+  
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open');
   }
 }
 
+
+
 /* START HERE: 
 
 */
  let articles = document.querySelectorAll('.article')
- articles.forEach(article => new Article(article))
+ let articlesArr = Array.from(articles)
+  TweenMax.staggerFrom(articlesArr, 2, {opacity: 0, x: -100, ease:Bounce.easeOut, delay: .5}, 0.4);
+  articles.forEach(article => {
+   return new Article(article);
+ })
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -2,8 +2,10 @@
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
   menu.classList.toggle('menu--open');
+  TweenMax.from(menu, 1, {x: -200})
     console.log('hamburger clicked');
 }
+
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
 console.log(menu);
